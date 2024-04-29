98560d0d389ff50a8bed7c1831907018795150b4
Pasuki <Pascal-Smigielski@outlook.de>
2024-04-29 19:34:00 +0200

    Firstt

--- src/main/java/net/pasuki/mccourse/MCCourseMod.java
+++ src/main/java/net/pasuki/mccourse/MCCourseMod.java
@@ -19,7 +19,7 @@ import org.slf4j.Logger;
 @Mod(MCCourseMod.MOD_ID)
 public class MCCourseMod
 {
-    // Define mod id in a common place for everything to reference
+    // Define mod id in a common place for everything to referencefyfgjygghhkj
     public static final String MOD_ID = "mccourse";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
